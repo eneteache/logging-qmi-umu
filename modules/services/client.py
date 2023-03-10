bb37ba06711c80ecbe7caa52b72a99762d06acd2
Anthony <anthony1997@hotmail.es>
2023-03-10 15:33:19 +0100

    update

--- modules/services/client.py
+++ modules/services/client.py
@@ -5,7 +5,6 @@ gi.require_version('Qmi', '1.0')
 from gi.repository import GLib, Gio, Qmi
 from modules.device.device import Device
 from modules import defines
-import logging
 
 class Client():
     
@@ -13,7 +12,7 @@ class Client():
 
     def __init__(self, qmidev, main_loop, service_type) -> None:
         
-        log = logging.getLogger("clients")
+        self.log = defines.log
 
         self.cid = 0
         self.qmidev = qmidev
@@ -21,9 +20,9 @@ class Client():
         self.service_type = service_type
         
         self.device = Device()
-        self.device.set_device_clients(self.device.get_device_clients()+1)
+        self.device.set_clients(self.device.get_clients()+1)
         
-        log.info("new {} client - ID: {} ".format(Qmi.service_get_string(self.service_type).upper(), self.device.get_device_clients()))
+        self.log.info("new {} client - ID: {} ".format(Qmi.service_get_string(self.service_type).upper(), self.device.get_clients()))
 
     #getters
     def get_cid(self):
@@ -48,13 +47,17 @@ class Client():
         self.num_requests = num_requests
         
 
-    def init(self):
+    def init(self, check_device_manual):
+
+
+        def sub_clients():
+            self.device.set_clients(self.device.get_clients()-1)
         def device_close_ready(qmidev,result,user_data=None):
             
             try:
                 qmidev.close_finish(result)
             except GLib.GError as error:
-                sys.stderr.write("Couldn't close QMI device: %s\n" % error.message)
+                defines.log.error("Couldn't close QMI device: %s\n" % error.message)
             main_loop.quit()
 
         def device_close(qmidev):
@@ -65,8 +68,9 @@ class Client():
             try:
                 qmidev.release_client_finish(result)
             except GLib.GError as error:
-                sys.stderr.write("Couldn't release QMI client: %s\n" % error.message)
+                defines.log.error("Couldn't release QMI client: %s\n" % error.message)
 
+            sub_clients()
 
         def release_client(qmidev,qmiclient):
             
@@ -79,28 +83,29 @@ class Client():
             try:
                 qmidev.close_finish(result)
             except GLib.GError as error:
-                sys.stderr.write("Couldn't close QMI device: %s\n" % error.message)
+                defines.log.error("Couldn't close QMI device: %s\n" % error.message)
             main_loop.quit()
 
         def allocate_client_ready(qmidev,result,user_data=None):
+            
             try:
                 qmiclient = qmidev.allocate_client_finish(result)
             except GLib.GError as error:
-                sys.stderr.write("Couldn't allocate QMI client: %s\n" % error.message)
-                device_close(qmidev)
-                return
+                defines.log.error("Couldn't allocate QMI client: %s\n" % error.message)
+                
+                user_data()
+                #device_close(qmidev)
+                return error
 
             if self.cid != qmiclient.get_cid():
                 cid = qmiclient.get_cid()
-                print("--- Init CID: {} - {}".format(cid, Qmi.service_get_string(service_type)))
+                #defines.log.info("--- Init CID: {} - {}".format(cid, Qmi.service_get_string(service_type)))
                 set_cid(cid)
-            
+            defines.elmundo = 2
             release_client(qmidev, qmiclient)
 
         service_type = self.service_type
         main_loop = self.get_main_loop()
         qmidev = self.get_qmidev()
-        qmidev.allocate_client(self.service_type, Qmi.CID_NONE, 10, None, allocate_client_ready, None)
 
-
-    
+        qmidev.allocate_client(self.service_type, Qmi.CID_NONE, 10, None, allocate_client_ready, check_device_manual)
